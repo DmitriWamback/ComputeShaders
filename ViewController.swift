cc28267da59c5147d9339142b36f25cb5dcf17a1
Dmitri Wamback <56200546+DmitriWamback@users.noreply.github.com>
2022-07-23 09:46:01 -0400

    .

--- ViewController.swift
+++ ViewController.swift
@@ -6,13 +6,14 @@ import simd
 class ViewController: NSViewController {
     
     var metal_device:     MTLDevice!
-    var metal_layer:      CAMetalLayer!
     var pipeline:         MTLRenderPipelineState!
     var queue:            MTLCommandQueue!
     var metalView:        MTKView!
     var compute_pipeline: MTLComputePipelineState!
     
-    var test:         Renderable!
+    var test:             Renderable!
+    static var uniforms:  Uniforms!
+    var kernel:           ComputeKernel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +22,32 @@ class ViewController: NSViewController {
         
         metal_device = MTLCreateSystemDefaultDevice()
         queue = metal_device.makeCommandQueue()
+        
+        let library  = metal_device.makeDefaultLibrary()
+        let vertex   = library?.makeFunction(name: "vertexMain")
+        let fragment = library?.makeFunction(name: "fragmentMain")
+        
+        let stateDescriptor = MTLRenderPipelineDescriptor()
+        stateDescriptor.vertexFunction = vertex
+        stateDescriptor.fragmentFunction = fragment
+        stateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        pipeline = try! metal_device.makeRenderPipelineState(descriptor: stateDescriptor)
+        
         metalView.delegate = self
         metalView.device = metal_device
         self.view.addSubview(metalView)
         self.view.frame.size = metalView.frame.size
         
+        ViewController.uniforms = Uniforms(color: SIMD3<Float>(1.0, 0.0, 0.5), window_size: SIMD2<Float>(Float(self.view.frame.width), Float(self.view.frame.height)), time: 0)
+        
+        kernel = ComputeKernel()
+        kernel.createComputeKernel(library: library, device: metal_device, commandQueue: queue)
+        
         test = Renderable()
         test.sendData(vertices: [-0.5, -0.5, 0.0,
                                   0.5, -0.5, 0.0,
-                                  0.0,  0.5, 0.0])
+                                  0.0,  0.5, 0.0], uniforms: ViewController.uniforms)
     }
 }
 
@@ -45,39 +63,28 @@ extension ViewController: MTKViewDelegate {
             return
         }
         
-        let library  = metal_device.makeDefaultLibrary()
-        let vertex   = library?.makeFunction(name: "vertexMain")
-        let fragment = library?.makeFunction(name: "fragmentMain")
-        let kernel   = library?.makeFunction(name: "compute")
-        
-        let stateDescriptor = MTLRenderPipelineDescriptor()
-        stateDescriptor.vertexFunction = vertex
-        stateDescriptor.fragmentFunction = fragment
-        stateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        
-        pipeline = try! metal_device.makeRenderPipelineState(descriptor: stateDescriptor)
-        compute_pipeline = try! metal_device.makeComputePipelineState(function: kernel!)
-        
         let renderPassDescriptor = MTLRenderPassDescriptor()
         renderPassDescriptor.colorAttachments[0].texture = drawable.texture
         renderPassDescriptor.colorAttachments[0].loadAction = .clear
         renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
         
-        guard let commandBuffer = queue.makeCommandBuffer() else {
-            return
-        }
-        
-        guard let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else {
-            return
-        }
+        guard let commandBuffer = queue.makeCommandBuffer() else { return }
+        guard let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { return }
         
         renderEncoder.setRenderPipelineState(pipeline)
+        renderEncoder.setFragmentTexture(kernel.texture, index: 0)
         
         test.render(renderEncoder: renderEncoder, device: metal_device)
         
         renderEncoder.endEncoding()
         commandBuffer.present(drawable)
         commandBuffer.commit()
+        
+        guard let drawable = view.currentDrawable else {
+            return
+        }
+        
+        kernel.use(drawable: drawable)
     }
 }
 
@@ -86,6 +93,7 @@ extension ViewController: MTKViewDelegate {
 
 struct Uniforms {
     var color: SIMD3<Float>
+    var window_size: SIMD2<Float>
     var time: Float
 }
 
@@ -94,9 +102,9 @@ class Renderable {
     var vertices: [Float]!
     var uniforms: Uniforms!
     
-    func sendData(vertices: [Float]) {
+    func sendData(vertices: [Float], uniforms: Uniforms) {
         self.vertices = vertices
-        uniforms = Uniforms(color: SIMD3<Float>(1.0, 0.0, 1.0), time: 1.0)
+        self.uniforms = uniforms
     }
     
     func makeVertexBuffer(device: MTLDevice) -> MTLBuffer? {
@@ -112,8 +120,55 @@ class Renderable {
         uniforms.time += 0.1
         renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3)
     }
+}
+
+class ComputeKernel {
     
-    func render(computeEncoder: MTLComputeCommandEncoder, device: MTLDevice) {
+    var computePipeline: MTLComputePipelineState!
+    var commandBuffer:   MTLCommandBuffer!
+    
+    var metal_device:    MTLDevice!
+    var library:         MTLLibrary!
+    var commandQueue:    MTLCommandQueue!
+    var texture:         MTLTexture!
+    
+    func createComputeKernel(library: MTLLibrary?, device: MTLDevice?, commandQueue: MTLCommandQueue) {
         
+        metal_device = device
+        self.library = library
+        self.commandQueue = commandQueue
+        
+        let computeKernel = library?.makeFunction(name: "compute")
+        do {
+            computePipeline = try device?.makeComputePipelineState(function: computeKernel!)
+        }
+        catch {
+            print(error)
+        }
+        
+        self.commandBuffer = self.commandQueue.makeCommandBuffer()
+        
+        let textureDescriptor = MTLTextureDescriptor()
+        textureDescriptor.storageMode = .private
+        textureDescriptor.usage = [.shaderRead, .shaderWrite]
+        textureDescriptor.pixelFormat = .rgba32Float
+        textureDescriptor.width = Int(ViewController.uniforms.window_size.x)
+        textureDescriptor.height = Int(ViewController.uniforms.window_size.y)
+        textureDescriptor.depth = 1
+        texture = device?.makeTexture(descriptor: textureDescriptor)
+    }
+    
+    func use(drawable: CAMetalDrawable?) {
+        
+        self.commandBuffer = self.commandQueue.makeCommandBuffer()
+        guard let computeEncoder = commandBuffer.makeComputeCommandEncoder() else {return}
+        computeEncoder.setComputePipelineState(computePipeline)
+        computeEncoder.setTexture(texture, index: 0)
+        computeEncoder.setBytes(&ViewController.uniforms, length: MemoryLayout<Uniforms>.stride, index: 1)
+        computeEncoder.dispatchThreads(MTLSize(width: 100, height: 1, depth: 1), threadsPerThreadgroup: MTLSize(width: 1, height: 1, depth: 1))
+        computeEncoder.endEncoding()
+        
+        commandBuffer.present(drawable!)
+        commandBuffer.commit()
     }
 }
